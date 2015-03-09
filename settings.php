0d9efcb2d5edad54949959e2002fc495cfb9986b
novicaa <novicamiketic@gmail.com>
2015-03-09 14:46:34 +0100

    First commit

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+	$adresa="The Centre for Chiropractic Health 2340 Highland Avenue,Suite 360 Lombard, IL 60148"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica=""; // AKo ne znate skracenicu ostavite praznu
+	$telephone=6302682680;// Broj koji se poziva na dugme CALL US
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="flip"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
